31c0e1a91062d79b8b14b5cbf343669d2defdfa5
sharifedak12 <shari.fedak@gmail.com>
2022-06-27 16:01:47 -0400

    Added tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,3 +20,66 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+import pytest
+from app import create_app
+from app.models.board import Board
+from app.models.card import Card
+from app import db
+from flask.signals import request_finished
+
+@pytest.fixture
+def app():
+    # create the app with a test config dictionary
+    app = create_app({"TESTING": True})
+
+    @request_finished.connect_via(app)
+    def expire_session(sender, response, **extra):
+        db.session.remove()
+
+    with app.app_context():
+        db.create_all()
+        yield app
+
+    # close and remove the temporary database
+    with app.app_context():
+        db.drop_all()
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture
+def one_card(app):
+    new_card = Card(
+        message="You can do it!", likes=0,)
+    db.session.add(new_card)
+    db.session.commit()
+
+@pytest.fixture
+def three_cards(app):
+    db.session.add_all([
+        Card(
+            message="You can do it!", likes=0),
+        Card(
+            message = "It's not that hard!", likes=0),
+        Card(
+            message = "I believe in you!", likes=0)
+    ])
+    db.session.commit()
+
+@pytest.fixture
+def one_board(app):
+    new_board = Board(title="Share your support", owner="Shari")
+    db.session.add(new_board)
+    db.session.commit()
+
+@pytest.fixture
+def one_card_belongs_to_one_board(app, one_board, one_card):
+    card = Card.query.first()
+    board = Board.query.first()
+    board.cards.append(card)
+    db.session.commit()
+
+
---
+++ tests/test_routes.py
@@ -0,0 +1,150 @@
+from app.models.board import Board
+from app.models.board import Card
+import pytest
+
+
+@pytest.mark.skip(reason="No way to test this feature yet")
+def test_get_boards_no_boards(client):
+    # Act
+    response = client.get("/boards")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == []
+
+
+@pytest.mark.skip(reason="No way to test this feature yet")
+def test_get_board_one_board(client, one_task):
+    # Act
+    response = client.get("/boards")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 1
+    assert response_body == [
+        {
+            "id": 1,
+            "title": "Share your support", 
+            "owner":"Shari"
+        }
+    ]
+
+
+@pytest.mark.skip(reason="No way to test this feature yet")
+def test_get_specific_board(client, one_task):
+    # Act
+    response = client.get("/boards/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == {
+        {
+            "id": 1,
+            "title": "Share your support", 
+            "owner":"Shari"
+        }
+    }
+
+
+@pytest.mark.skip(reason="No way to test this feature yet")
+def test_get_task_not_found(client):
+    # Act
+    response = client.get("/boards/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 404
+    assert response_body == {'details': 'Invalid id: 1'}
+
+
+@pytest.mark.skip(reason="No way to test this feature yet")
+def test_create_board(client):
+    # Act
+    response = client.post("/boards", json={
+        "title": "It's a new board",
+        "owner": "Ada Lovelace",
+    })
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 201
+    assert response_body == {
+    {
+            "id": 1,
+            "title": "It's a new board",
+            "owner": "Ada Lovelace",
+        }
+    }
+    new_board = Board.query.get(1)
+    assert new_board
+    assert new_board.title == "It's a new board"
+    assert new_board.description == "Ada Lovelace"
+
+@pytest.mark.skip(reason="No way to test this feature yet")
+def test_delete_card(client, one_card):
+    # Act
+    response = client.delete("/cards/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == {
+        "details": 'Card successfully deleted'
+    }
+    assert Card.query.get(1) == None
+
+
+@pytest.mark.skip(reason="No way to test this feature yet")
+def test_delete_card_not_found(client):
+    # Act
+    response = client.delete("/cards/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 404
+    assert response_body == {'details': 'Invalid id: 1'}
+    assert Card.query.all() == []
+
+
+@pytest.mark.skip(reason="No way to test this feature yet")
+def test_create_card_must_contain_message(client):
+    # Act
+    response = client.post("/cards", json={
+    })
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 400
+    assert "details" in response_body
+    assert response_body == {
+        "details": "Invalid data"
+    }
+    assert Card.query.all() == []
+
+
+@pytest.mark.skip(reason="No way to test this feature yet")
+def test_get_cards_for_specific_board(client, one_card_belongs_to_one_board):
+    # Act
+    response = client.get("/boards/1/cards")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert "cards" in response_body
+    assert len(response_body["tasks"]) == 1
+    assert response_body == {
+        "id": 1,
+        "title": "Share your support",
+        "owner": "Shari",
+        "cards": [
+            {
+                "id": 1,
+                "board_id": 1,
+                "message":"You can do it!",
+                "likes": 0,
+            }
+        ]
+    }
