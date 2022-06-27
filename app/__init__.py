7b81608c927e7cc5222700b9e67b4346b2fee8cf
Myranae <myranae@pm.me>
2022-06-27 15:14:15 -0400

    adds Board to init

--- app/__init__.py
+++ app/__init__.py
@@ -19,9 +19,12 @@ def create_app():
 
     # Import models here for Alembic setup
     # from app.models.ExampleModel import ExampleModel
+    from app.models.board import Board
 
     db.init_app(app)
     migrate.init_app(app, db)
+
+
 
     # Register Blueprints here
     # from .routes import example_bp
