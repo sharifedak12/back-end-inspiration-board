05e77712a937a1aa9b427f4c34b4187dc64148ed
Natalia <natalia.woodson@gmail.com>
2022-06-28 14:30:37 -0400

    corrected typo in bp registration

--- app/__init__.py
+++ app/__init__.py
@@ -20,6 +20,7 @@ def create_app():
     # Import models here for Alembic setup
     # from app.models.ExampleModel import ExampleModel
     from app.models.board import Board
+    from app.models.card import Card
 
     db.init_app(app)
     migrate.init_app(app, db)
@@ -29,7 +30,7 @@ def create_app():
     # Register Blueprints here
     # from .routes import example_bp
     # app.register_blueprint(example_bp)
-    from .board_routes import boards_bp
-    app.register_blueprint(boards_bp)
+    from .board_routes import board_bp
+    app.register_blueprint(board_bp)
     CORS(app)
     return app
