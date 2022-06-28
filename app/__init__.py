f7149db60d15d597f467bbb1c07cc75ebf289ed7
sharifedak12 <shari.fedak@gmail.com>
2022-06-28 14:02:10 -0400

    update __init__

--- app/__init__.py
+++ app/__init__.py
@@ -35,7 +35,7 @@ def create_app(test_config=None):
     # Register Blueprints here
     # from .routes import example_bp
     # app.register_blueprint(example_bp)
-    from .routes import cards
+    from .card_routes import cards
     app.register_blueprint(cards)
     
     CORS(app)
