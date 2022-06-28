d0dc42ecfbffc1736174f8281a8b24d359982f4a
Myranae <myranae@pm.me>
2022-06-28 16:27:41 -0400

    cleans code by removing comments

--- app/routes/board_routes.py
+++ app/routes/board_routes.py
@@ -5,13 +5,8 @@ from app.routes.helper_routes import validate_id, validate_request
 
 boards_bp = Blueprint("boards_bp", __name__, url_prefix="/boards")
 
-# View a list of cards that belong to the selected board.
-# need to add validate_id helper function
-# need to add get_cards method to board
 @boards_bp.route("/<board_id>/cards", methods=["GET"])
 def get_cards_from_one_board(board_id):
     board = validate_id(Board, board_id)
     response = board.to_dict()
-    # response["cards"] = board.get_cards()
-    # return make_response(response)
     return make_response({"cards": response["cards"]}, 201)
