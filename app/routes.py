ad9dd74f4e07761f99634cb609c66c7821dc76bc
sharifedak12 <shari.fedak@gmail.com>
2022-06-28 13:17:24 -0400

    removed add_card from Board

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,17 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
+from app.models.card import Card
 
 # example_bp = Blueprint('example_bp', __name__)
+cards = Blueprint('cards', __name__)
+
+@cards.route('/cards', methods=['POST'])
+def create_card():
+    """
+    Create a new card
+    """
+    data = request.get_json()
+    card = Card(data['message'], data['board_id']).create()
+    db.session.add(card)
+    db.session.commit()
+    return jsonify(card.to_dict())
